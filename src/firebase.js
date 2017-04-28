779e2bd831981bc613baeef6d49f6912049aef0b
Pira <sritharan.pirathaab@gmail.com>
2017-04-28 18:24:09 +0200

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyCvAy_yMNc9W3uaUOEeW5S7LAIaS3D6m1k",
+    authDomain: "goal-coach-af2f9.firebaseapp.com",
+    databaseURL: "https://goal-coach-af2f9.firebaseio.com",
+    projectId: "goal-coach-af2f9",
+    storageBucket: "goal-coach-af2f9.appspot.com",
+    messagingSenderId: "629640767342"
+};
+
+export const firebaseApp = firebase.initializeApp(config);
+
+export const goalRef = firebase.database().ref('goals');
